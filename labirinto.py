e5a409ecf869da5e247ec9ed12d0b8368aec19c1
taysa-fernandes <taysamirelly34@gmail.com>
2023-11-07 16:34:57 -0300

    rato se movendo pelas setas do teclado

--- labirinto.py
+++ labirinto.py
@@ -11,7 +11,7 @@ BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 
 imagem_mouse = pygame.image.load("rato1.jpg")
-imagem_queijo = pygame.image.load("queijo.jpg")  
+imagem_queijo = pygame.image.load("queijo.jpg")
 
 def carregar_labirinto(arquivo):
     with open(arquivo, "r") as arquivo_labirinto:
@@ -41,14 +41,37 @@ posicao_objetivo_x, posicao_objetivo_y = None, None
 imagens_celulas = {
     1: pygame.Surface((TAMANHO_CELULA, TAMANHO_CELULA)),
     2: imagem_mouse,
-    3: imagem_queijo,  
+    3: imagem_queijo,
 }
+
+for y, linha in enumerate(labirinto):
+    for x, celula in enumerate(linha):
+        if celula == 2:  # Encontre a posição inicial do jogador
+            posicao_jogador_x = x
+            posicao_jogador_y = y
 
 executando = True
 while executando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             executando = False
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                if posicao_jogador_x > 0 and labirinto[posicao_jogador_y][posicao_jogador_x - 1] != 1:
+                    labirinto[posicao_jogador_y][posicao_jogador_x] = 0  # Apaga a posição anterior
+                    posicao_jogador_x -= 1
+            if evento.key == pygame.K_RIGHT:
+                if posicao_jogador_x < len(labirinto[0]) - 1 and labirinto[posicao_jogador_y][posicao_jogador_x + 1] != 1:
+                    labirinto[posicao_jogador_y][posicao_jogador_x] = 0
+                    posicao_jogador_x += 1
+            if evento.key == pygame.K_UP:
+                if posicao_jogador_y > 0 and labirinto[posicao_jogador_y - 1][posicao_jogador_x] != 1:
+                    labirinto[posicao_jogador_y][posicao_jogador_x] = 0
+                    posicao_jogador_y -= 1
+            if evento.key == pygame.K_DOWN:
+                if posicao_jogador_y < len(labirinto) - 1 and labirinto[posicao_jogador_y + 1][posicao_jogador_x] != 1:
+                    labirinto[posicao_jogador_y][posicao_jogador_x] = 0
+                    posicao_jogador_y += 1
 
     TELA.fill(BRANCO)
 
@@ -57,6 +80,8 @@ while executando:
             imagem = imagens_celulas.get(celula, None)
             if imagem is not None:
                 TELA.blit(imagem, (x * TAMANHO_CELULA, y * TAMANHO_CELULA))
+
+    TELA.blit(imagem_mouse, (posicao_jogador_x * TAMANHO_CELULA, posicao_jogador_y * TAMANHO_CELULA))
 
     pygame.display.update()
 
